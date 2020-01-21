68f41adff4c986822b905c4f4edc9ad7d760dbab
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-01-21 20:32:31 -0300

    BBL-41 creating folder for organizations layer

---
+++ organizations/variables.tf
@@ -0,0 +1,32 @@
+#
+# AWS Provider Settings
+#
+variable "region" {
+  description = "AWS Region"
+}
+
+variable "profile" {
+  description = "AWS Profile"
+}
+
+#===========================================#
+# Project Variables                         #
+#===========================================#
+variable "project" {
+  description = "Project Name"
+}
+
+variable "project_long" {
+  description = "Project Long Name"
+}
+
+variable "environment" {
+  description = "Environment Name"
+  default = "dev"
+}
+
+#
+# Backend Variables -- because TF12 will not allow you to omit these :(
+#
+variable "bucket" {}
+variable "encrypt" {}
