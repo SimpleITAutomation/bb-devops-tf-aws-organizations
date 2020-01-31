091ca02674604fa65db57c9ca41be6e03c1469ec
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-01-31 15:42:26 -0300

    BBL-41 Updating from dev to apps-devstg convention + other minor updates

--- organizations/variables.tf
+++ organizations/variables.tf
@@ -1,32 +1,60 @@
 #
-# AWS Provider Settings
+# config/backend.config
 #
+#================================#
+# Terraform AWS Backend Settings #
+#================================#
 variable "region" {
+  type        = string
   description = "AWS Region"
 }
 
 variable "profile" {
-  description = "AWS Profile"
+  type        = string
+  description = "AWS Profile (required by the backend but also used for other resources)"
 }
 
-#===========================================#
-# Project Variables                         #
-#===========================================#
+variable "bucket" {
+  type        = string
+  description = "AWS S3 TF State Backend Bucket"
+}
+variable "dynamodb_table" {
+  type        = string
+  description = "AWS DynamoDB TF Lock state table name"
+}
+variable "encrypt" {
+  type        = bool
+  description = "Enable AWS DynamoDB with server side encryption"
+}
+
+#
+# config/base.config
+#
+#=============================#
+# Project Variables           #
+#=============================#
 variable "project" {
+  type        = string
   description = "Project Name"
 }
 
 variable "project_long" {
+  type        = string
   description = "Project Long Name"
 }
 
 variable "environment" {
+  type        = string
   description = "Environment Name"
-  default     = "dev"
 }
 
 #
-# Backend Variables -- because TF12 will not allow you to omit these :(
+# config/extra.config
 #
-variable "bucket" {}
-variable "encrypt" {}
+#=============================#
+# Accounts & Extra Vars       #
+#=============================#
+variable "region_secondary" {
+  type        = string
+  description = "AWS Scondary Region for HA"
+}
